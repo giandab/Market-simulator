d34e7ac5cd43255ddab543e185301def8d9095ee
John Dabos Doukas <giandabdouk@gmail.com>
2022-10-22 17:08:08 +0100

    Started working with yahoo finance

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+import yfinance as YahooFin
+
 def main():
-    user_input=input()
-    
+    user_input=input("Type '-info [ticker]' to get general information")
+    if user_input[0:5]=="-info":
+        get_ticker_info = YahooFin.Ticker(user_input[6:])
+        general_stock_info(get_ticker_info)
+    if user_input=="exit":
+        quit()
+
+def general_stock_info(ticker):
+    for key,value in ticker.info.items():
+        print(key, ":", value)
+    main()
+
+main()
