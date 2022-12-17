d65fbfcbd9e7e662186c85195306dc6f44ee9722
John Dabos Doukas <giandabdouk@gmail.com>
2022-12-17 21:50:05 +0000

    tidy up graph

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 def main():
 
     # Main input section
-    user_input=input("Type '-info [ticker]' to get general information" + "/n" + "Type -graph [ticker] [time range] to get a price graph for times: 1d, 5d, 1mo, 3mo, 6mo, 1y, 2y, 5y, 10y and ytd." )
+    user_input=input("Type '-info [ticker]' to get general information" + "\n" + "Type -graph [ticker] [time range] to get a price graph for times: 1d, 5d, 1mo, 3mo, 6mo, 1y, 2y, 5y, 10y and ytd." )
 
     # For general infromation
     if user_input[0:5]=="-info":
@@ -19,20 +19,26 @@ def main():
         ticker_string = ticker_range_string.partition(" ")[0]
         ticker = YahooFin.Ticker(ticker_string)
         time_range = ticker_range_string.partition(" ")[2]
-        price_graph(ticker, time_range)
+        price_graph(ticker, time_range,ticker_string)
 
     if user_input=="exit":
         quit()
 
+#Get info list for a certain product
 def general_stock_info(ticker):
     for key,value in ticker.info.items():
         print(key, ":", value)
     main()
 
-def price_graph(ticker, time):
+
+#Display graph of price over time
+def price_graph(ticker, time,ticker_string):
     gr = ticker.history(time)
     print(gr)
     gr.plot(kind='line', y='Close')
+    plt.title(ticker_string)
+    plt.xlabel("Date")
+    plt.ylabel("Price")
     plt.show()
 
 main()
