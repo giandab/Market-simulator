aa4f00b384aa17624eeca36f82d0171e01a8771c
Ioannis <giandabdouk@gmail.com>
2022-12-22 23:07:13 +0000

    basic buy/sell

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ import matplotlib.pyplot as plt
 
 def main():
 
+    positions = []
+
     # Main input section
-    user_input=input("Type '-info [ticker]' to get general information" + "\n" + "Type -graph [ticker] [time range] to get a price graph for times: 1d, 5d, 1mo, 3mo, 6mo, 1y, 2y, 5y, 10y and ytd." )
+    user_input=input("Type '-info [ticker]' to get general information" + "\n" + "Type -graph [ticker] [time range] to get a price graph for times: 1d, 5d, 1mo, 3mo, 6mo, 1y, 2y, 5y, 10y and ytd." + "\n" + "Type -open [ticker] [amount] to buy or sell"+ "\n" +"Type -exit to end the program" )
 
     # For general infromation
     if user_input[0:5]=="-info":
-        get_ticker_info = YahooFin.Ticker(user_input[6:])
+        get_ticker_info = YahooFin.Ticker(user_input[6::])
         general_stock_info(get_ticker_info)
 
     # For plotting
@@ -21,7 +23,15 @@ def main():
         time_range = ticker_range_string.partition(" ")[2]
         price_graph(ticker, time_range,ticker_string)
 
-    if user_input=="exit":
+    #For buying
+    elif user_input[0:5]=="-open":
+        ticker_range_string = user_input[6::]
+        ticker_string = ticker_range_string.partition(" ")[0]
+        ticker = YahooFin.Ticker(ticker_string)
+        amount = ticker_range_string.partition(" ")[2]
+        positions.append(Position(ticker_string,amount,ticker.info['regularMarketPrice']))
+
+    elif user_input=="-exit":
         quit()
 
 #Get info list for a certain product
@@ -40,5 +50,27 @@ def price_graph(ticker, time,ticker_string):
     plt.xlabel("Date")
     plt.ylabel("Price")
     plt.show()
+    main()
+
+
+class Position:
+    def __init__(self, product, amount, purchase_price):
+        self.product= product
+        self.amount = amount
+        self.purchase_price = purchase_price
+
+    def profit_loss(self):
+        ticker = YahooFin.Ticker(self.product)
+        current_price = ticker.info['regularMarketPrice']
+        profit_loss = self.amount*(current_price - self.purchase_price)
+        return profit_loss
+
+    def percentage_change(self):
+        ticker = YahooFin.Ticker(self.product)
+        current_price = ticker.info['regularMarketPrice']
+        if self.purchase_price > current_price:
+            return (-1+(current_price/self.purchase_price))
+        else:
+            return 1- (self.purchase_price/current_price)
 
 main()
