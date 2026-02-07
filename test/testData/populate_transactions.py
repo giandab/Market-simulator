ceff2309a0456e4e1fd87c38e0a9ab609f79e039
giandab <giandabdouk@gmail.com>
2026-02-07 14:42:03 +0000

    test timestamp format

--- test/testData/populate_transactions.py
+++ test/testData/populate_transactions.py
@@ -1,10 +1,10 @@
-testdata = [{"product":"cash","amount":10000, "price":1 , "timestamp":"22/08/2025 10:00:00.00"},
-            {"product":"cash","amount":-2000, "price":1 , "timestamp":"22/08/2025 10:00:00.00"},
-            {"product":"AAPL","amount":2, "price":150 , "timestamp":"22/08/2025 10:00:00.00"},
-            {"product":"TSLA","amount":3, "price":200 , "timestamp":"22/08/2025 10:00:00.00"},
-            {"product":"TSLA","amount":1, "price":200 , "timestamp":"22/08/2025 10:00:00.00"},
-            {"product":"MSFT","amount":1, "price":100 , "timestamp":"23/08/2025 10:00:00.00"},
-            {"product":"MSFT","amount":3, "price":200 , "timestamp":"23/08/2025 11:00:00.00"},
-            {"product":"TSLA","amount":-1, "price":200 , "timestamp":"22/08/2025 10:00:00.00"},
-            {"product":"CA.PA","amount":1, "price":10, "timestamp":"22/08/2025 10:00:00.00"}
+testdata = [{"product":"cash","amount":10000, "price":1 , "timestamp":"2025-08-22 10:00:00.00"},
+            {"product":"cash","amount":-2000, "price":1 , "timestamp":"2025-08-22 10:00:00.00"},
+            {"product":"AAPL","amount":2, "price":150 , "timestamp":"2025-08-22 10:00:00.00"},
+            {"product":"TSLA","amount":3, "price":200 , "timestamp":"2025-08-22 10:00:00.00"},
+            {"product":"TSLA","amount":1, "price":200 , "timestamp":"2025-08-22 10:00:00.00"},
+            {"product":"MSFT","amount":1, "price":100 , "timestamp":"2025-08-23 10:00:00.00"},
+            {"product":"MSFT","amount":3, "price":200 , "timestamp":"2025-08-23 11:00:00.00"},
+            {"product":"TSLA","amount":-1, "price":200 , "timestamp":"2025-08-22 10:00:00.00"},
+            {"product":"CA.PA","amount":1, "price":10, "timestamp":"2025-08-22 10:00:00.00"}
             ]
